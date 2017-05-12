a9fa73091ffcf3277b3b905f604e541a282668af
Artem Novikov <novikov@i-market.ru>
2017-05-12 18:59:19 +0300

    homepage mockup

--- public/local/templates/main/footer.php
+++ public/local/templates/main/footer.php
@@ -1,5 +1,49 @@
-<? if (!\App\App::useBitrixAsset()): ?>
-    <? foreach (\App\App::assets()['scripts'] as $path): ?>
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+use App\App;
+use App\AppController;
+use App\View as v;
+
+extract(AppController::layoutContext(), EXTR_SKIP)
+?>
+</main>
+<!-- FOOTER START -->
+<footer class="footer">
+    <div class="wrap">
+        <div class="top">
+            <a class="logo_footer" href="<?= v::path('/') ?>">
+                <img src="<?= v::asset('images/logo_footer.png') ?>" alt="Техническая строительная экспертиза">
+                <p>
+                    <span>Техническая</span>
+                    <span>строительная экспертиза</span>
+                </p>
+            </a>
+            <? // TODO link ?>
+            <a class="advertisers_hidden" href="#">Рекламодателям</a>
+            <div class="adress">
+                <p>111141 г. Москва, 3-й проезд Перова Поля, <br> дом 8, строение 11, офис 402 <br> Станция метро "Перово", <br> последний вагон из центра (10 мин. пешком)</p>
+            </div>
+            <div class="info">
+                <p><a href="tel:+7 (495) 641-70-69">+7 (495) 641-70-69</a></p>
+                <p><a href="tel:+7 (499) 340-34-73">+7 (499) 340-34-73</a></p>
+                <p><span>E-mail:</span><a href="mailto:6417069@bk.ru">6417069@bk.ru</a></p>
+                <p><span>E-mail:</span><a href="mailto:6417069@bk.ru">6417069@bk.ru</a></p>
+            </div>
+            <div class="btns">
+                <div class="blue_btn re_call">Заказать <span class="hidden">обратный</span> звонок</div>
+                <div class="blue_btn calculate_cost">Рассчитать стоимость</div>
+            </div>
+        </div>
+        <div class="bottom">
+            <? // TODO includize copyright? ?>
+            <span><?= '© 2015–'.$copyrightYear ?></span> <span>ООО «Техническая строительная экспертиза»</span> <a href="https://i-market.ru/" target="_blank" class="create">Создание и продвижение сайта I-Market</a>
+            <? // TODO link ?>
+            <a class="advertisers" href="#">Рекламодателям</a>
+        </div>
+    </div>
+</footer>
+<? if (!App::useBitrixAsset()): ?>
+    <? foreach (App::assets()['scripts'] as $path): ?>
         <script type="text/javascript" src="<?= $path ?>"></script>
     <? endforeach ?>
 <? endif ?>
--- public/local/templates/main/header.php
+++ public/local/templates/main/header.php
@@ -1,7 +1,11 @@
-<?php
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
 use App\App;
+use App\AppController;
 use Bitrix\Main\Page\Asset;
+use App\View as v;
+
+extract(AppController::layoutContext(), EXTR_SKIP);
 
 $assets = App::assets();
 $asset = Asset::getInstance();
@@ -38,3 +42,133 @@ if (App::useBitrixAsset()) {
 </head>
 <body>
 <? $APPLICATION->ShowPanel() ?>
+<!--социалки-->
+<ul class="social">
+    <? // TODO ?>
+    <li>
+        <a href="#" class="vk"></a>
+    </li>
+    <li>
+        <a href="#" class="fb"></a>
+    </li>
+    <li>
+        <a href="#" class="od"></a>
+    </li>
+    <li>
+        <a href="#" class="tw"></a>
+    </li>
+    <li>
+        <a href="#" class="gp"></a>
+    </li>
+</ul>
+<!--прокрутка вверх-->
+<a class="scroll_top" href="#"></a>
+<!-- HEADER START -->
+<header class="header">
+    <div class="top">
+        <div class="wrap">
+            <div class="left">
+                <a class="logo" href="<?= v::path('/') ?>">
+                    <img src="<?= v::asset('images/logo.png') ?>" alt="Техническая строительная экспертиза">
+                    <p>
+                        <span>Техническая</span>
+                        <span>строительная экспертиза</span>
+                    </p>
+                </a>
+                <div class="inner">
+                    <div class="operating_schedule">
+                        <? $APPLICATION->IncludeComponent(
+                        	"bitrix:main.include",
+                        	"",
+                        	Array(
+                        		"AREA_FILE_SHOW" => "file",
+                        		"PATH" => v::includedArea('operating_schedule.php')
+                        	)
+                        ); ?>
+                    </div>
+                    <? // TODO implement search ?>
+                    <form action="" method="post" id="">
+                        <input type="text" placeholder="Найти">
+                        <button type="submit"></button>
+                    </form>
+                </div>
+                <div class="hamburger">
+                    <span></span><span></span><span></span>
+                </div>
+            </div>
+            <div class="right">
+                <div class="btns">
+                    <? // TODO modals ?>
+                    <div class="blue_btn re_call">Заказать <span class="hidden">обратный</span> звонок</div>
+                    <div class="blue_btn calculate_cost">Рассчитать стоимость</div>
+                </div>
+                <? // TODO contact details ?>
+                <div class="info">
+                    <div class="info_top">
+                        <p><a href="tel:+7(499) 340-34-73">+7(495) 641-70-69</a></p>
+                        <p><a href="tel:+7(499) 340-34-73">+7(499) 340-34-73</a></p>
+                    </div>
+                    <div class="info_bottom">
+                        <p>
+                            <span>E-mail:</span>
+                            <a href="mailto:6417069@bk.ru">6417069@bk.ru</a>
+                        </p>
+                        <p>
+                            <span>E-mail:</span>
+                            <a href="mailto:6417069@bk.ru">6417069@bk.ru</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <? $APPLICATION->IncludeComponent(
+            "bitrix:menu",
+            "header",
+            Array(
+                "ALLOW_MULTI_SELECT" => "N",
+                "CHILD_MENU_TYPE" => "left",
+                "DELAY" => "N",
+                "MAX_LEVEL" => "1",
+                "MENU_CACHE_GET_VARS" => array(""),
+                "MENU_CACHE_TIME" => "3600",
+                "MENU_CACHE_TYPE" => "N",
+                "MENU_CACHE_USE_GROUPS" => "Y",
+                "ROOT_MENU_TYPE" => "top",
+                "USE_EXT" => "Y"
+            )
+        ); ?>
+    </div>
+</header>
+<!-- CONTENT START -->
+<main class="content">
+    <? // TODO slider ?>
+    <section class="wrap_banner_slider">
+        <span class="arrow prev"></span>
+        <span class="arrow next"></span>
+        <div class="banner_slider">
+            <div class="slide" style="background: url('<?= v::asset('images/pic_1.jpg') ?>')no-repeat center center / cover">
+                <div class="wrap">
+                    <div class="left">
+                        <p>Техническая</p>
+                        <strong>строительная экспертиза</strong>
+                    </div>
+                    <div class="right">
+                        <p>Мы предоставляем услуги по проведению независимой строительно-технических экспертизы, а также строительной судебной и досудебной экспертизы. Наша деятельность осуществляется в соответствии с требованиями строительных норм, и в соответствии с законодательством Российской Федерации.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="slide" style="background: url('<?= v::asset('images/pic_1.jpg') ?>')no-repeat center center / cover">
+                <div class="wrap">
+                    <div class="left">
+                        <p>Техническая</p>
+                        <strong>строительная экспертиза</strong>
+                    </div>
+                    <div class="right">
+                        <p>Мы предоставляем услуги по проведению независимой строительно-технических экспертизы, а также строительной судебной и досудебной экспертизы. Наша деятельность осуществляется в соответствии с требованиями строительных норм, и в соответствии с законодательством Российской Федерации.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>

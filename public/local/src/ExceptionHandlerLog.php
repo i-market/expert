b0360c94e94ad99556ffab30e82e3293f4f9b7eb
Artem Novikov <novikov@i-market.ru>
2017-05-15 12:31:34 +0300

    array syntax

--- public/local/src/ExceptionHandlerLog.php
+++ public/local/src/ExceptionHandlerLog.php
@@ -13,23 +13,23 @@ class ExceptionHandlerLog extends \Bitrix\Main\Diag\ExceptionHandlerLog {
         global $USER;
         if (function_exists('curl_init')) {
             if (is_object($USER)) {
-                $this->client->user_context(array(
+                $this->client->user_context([
                     'id' => $USER->GetID(),
                     'username' => $USER->GetLogin(),
                     'email' => $USER->GetEmail()
-                ));
+                ]);
             }
-            $this->client->captureException($exception, array(
+            $this->client->captureException($exception, [
                 'logType' => $logType
-            ));
+            ]);
         }
     }
 
     public function initialize(array $options) {
         $dsn = _::get(Configuration::getValue('app'), 'sentry.dsn');
         /** @noinspection PhpUnnecessaryFullyQualifiedNameInspection */
-        $this->client = new Raven_Client($dsn, array(
+        $this->client = new Raven_Client($dsn, [
             'environment' => \App\App::env()
-        ));
+        ]);
     }
 }

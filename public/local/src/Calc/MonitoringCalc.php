e29052e78cff84d82101c376c12c8347567312ee
Artem Novikov <novikov@i-market.ru>
2017-05-18 16:42:56 +0300

    calc draft: add conditional multiplier parsing

--- public/local/src/Calc/MonitoringCalc.php
+++ public/local/src/Calc/MonitoringCalc.php
@@ -45,14 +45,35 @@ class MonitoringCalc extends AbstractCalc {
                 'STARTS_WITH' => 'Местонахождение'
             ],
 //            'Адрес',
-//            'Назначение объекта мониторинга',
+            [
+                'KEY' => 'USED_FOR',
+                'STARTS_WITH' => 'Назначение объекта мониторинга',
+            ],
 //            'Общая площадь объекта (кв.м.)',
-//            'Строительный объем объекта (куб. м.)',
-//            'Количество надземных этажей',
-//            'Наличие технического подполья, подвала, подземных этажей',
-//            'Количество подземных этажей',
-//            'Цели мониторинга',
-//            'Конструкции подлежащие мониторингу',
+            [
+                'KEY' => 'VOLUME',
+                'STARTS_WITH' => 'Строительный объем объекта (куб. м.)',
+            ],
+            [
+                'KEY' => 'FLOORS',
+                'STARTS_WITH' => 'Количество надземных этажей',
+            ],
+            [
+                'KEY' => 'HAS_UNDERGROUND_FLOORS',
+                'STARTS_WITH' => 'Наличие технического подполья, подвала, подземных этажей',
+            ],
+            [
+                'KEY' => 'UNDERGROUND_FLOORS',
+                'STARTS_WITH' => 'Количество подземных этажей',
+            ],
+            [
+                'KEY' => 'MONITORING_GOAL',
+                'STARTS_WITH' => 'Цели мониторинга',
+            ],
+            [
+                'KEY' => 'STRUCTURES_TO_MONITOR',
+                'STARTS_WITH' => 'Конструкции подлежащие мониторингу',
+            ],
 //            'Продолжетельность мониторинга (мес.)',
             // TODO Удаленность объектов друг от друга?
 //            'Удаленность объектов друг от друга',
@@ -89,7 +110,9 @@ class MonitoringCalc extends AbstractCalc {
                 }
             } elseif ($stateName === 'in_section') {
                 list($_, $section) = $state;
-                if ($isEmptyRow($cells)) {
+                // TODO если проверять только первую ячейку можно потерять (плохо составленные) данные,
+                // лучше чтобы вся строка была пустой
+                if (str::isEmpty(_::first($cells))) {
                     $state = ['find_section'];
                 } else {
                     $sectionKey2Rows[$section['KEY']][] = $cells;
@@ -101,14 +124,58 @@ class MonitoringCalc extends AbstractCalc {
             $normalized = str::replace($str, ',', '.');
             return floatval($normalized);
         };
-        $ret = _::map($sectionKey2Rows, function($rows, $sectionKey) use ($parseFloat) {
-            // basic case
-            $map = [];
-            foreach ($rows as $cells) {
-                list($k, $v) = $cells;
-                $map[$k] = $parseFloat($v);
+        $nonEmptyCells = function($cells) {
+            return array_filter($cells, function($cell) {
+                return !str::isEmpty($cell);
+            });
+        };
+        $ret = _::map($sectionKey2Rows, function($rows, $sectionKey) use ($parseFloat, $nonEmptyCells) {
+            if ($sectionKey === 'STRUCTURES_TO_MONITOR') {
+                $map = [];
+                $state = ['default'];
+                foreach ($rows as $idx => $cells) {
+                    $stateName = _::first($state);
+                    if ($stateName === 'default') {
+                        $isAllCaps = str::upper(_::first($cells)) === _::first($cells);
+                        $isSubsectionName = $isAllCaps;
+                        if (_::first($cells) === 'ВЫБОРОЧНЫЙ МОНИТОРИНГ') {
+                            $header = _::drop($nonEmptyCells($cells), 1);
+                            $state = ['in_conditional_multipliers', $header];
+                        } elseif ($isSubsectionName) {
+                            // TODO keep subsections for form rendering
+                            continue;
+                        } else {
+                            // simple case
+                            list($k, $v) = $cells;
+                            $map[$k] = $parseFloat($v);
+                        }
+                    } elseif ($stateName === 'in_conditional_multipliers') {
+                        list($_, $header) = $state;
+                        $filteredCells = $nonEmptyCells($cells);
+                        $isConditionalMultiplier = function($cells) use ($header) {
+                            return count($cells) === count($header) + 1;
+                        };
+                        if ($isConditionalMultiplier($filteredCells)) {
+                            $key = _::first($cells);
+                            $multipliers = array_map($parseFloat, _::drop($filteredCells, 1));
+                            $map[$key] = array_combine($header, $multipliers);
+                        }
+                        // peek the next row
+                        if (isset($rows[$idx + 1]) && !$isConditionalMultiplier($nonEmptyCells($rows[$idx + 1]))) {
+                            $state = ['default'];
+                        }
+                    }
+                }
+                return $map;
+            } else {
+                // simple case
+                $map = [];
+                foreach ($rows as $cells) {
+                    list($k, $v) = $cells;
+                    $map[$k] = $parseFloat($v);
+                }
+                return $map;
             }
-            return $map;
         });
         // TODO validate return value
         return ['MULTIPLIERS' => $ret];

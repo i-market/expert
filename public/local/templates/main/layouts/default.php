13b965b9f5a7760fae1a94a159bd6a474655d736
Artem Novikov <novikov@i-market.ru>
2017-05-19 17:29:12 +0300

    avoid `AddBufferContent` in default layout template (fixes buffering issues)

--- public/local/templates/main/layouts/default.php
+++ public/local/templates/main/layouts/default.php
@@ -1,7 +1,7 @@
 <section>
     <div class="wrap">
         <div class="wrap_title">
-            <h2><? $APPLICATION->ShowTitle(false) ?></h2>
+            <h2><?= $APPLICATION->GetTitle(false) ?></h2>
         </div>
         <?= $content ?>
     </div>

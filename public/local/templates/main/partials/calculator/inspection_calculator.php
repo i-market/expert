4d36f57158633d89929154676de003aaa95e0baf
Artem Novikov <novikov@i-market.ru>
2017-07-17 14:57:22 +0300

    remove empty icon img[alt]

--- public/local/templates/main/partials/calculator/inspection_calculator.php
+++ public/local/templates/main/partials/calculator/inspection_calculator.php
@@ -100,7 +100,7 @@ $macros = new macros($state);
                 <button type="submit" class="big_btn">
                     <span class="text"><span>Выполнить расчет</span></span>
                     <span class="img">
-    <img src="<?= v::asset('images/calc.svg') ?>" alt="">
+    <img src="<?= v::asset('images/calc.svg') ?>">
   </span>
                 </button>
             </div>

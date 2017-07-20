03fd70975b272e72f1e67d933c3dc8d9c085923b
Artem Novikov <novikov@i-market.ru>
2017-07-20 19:26:57 +0300

    fix missing property code in news.list params

--- public/local/src/Components.php
+++ public/local/src/Components.php
@@ -60,7 +60,7 @@ class Components {
                 "PARENT_SECTION" => "",
                 "PARENT_SECTION_CODE" => $iblockSection,
                 "PREVIEW_TRUNCATE_LEN" => "",
-                "PROPERTY_CODE" => array("", ""),
+                "PROPERTY_CODE" => array("LINK"),
                 "SET_BROWSER_TITLE" => "N",
                 "SET_LAST_MODIFIED" => "N",
                 "SET_META_DESCRIPTION" => "N",

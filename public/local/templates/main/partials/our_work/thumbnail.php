393da6efbdf79269272bfe877524063502d26936
Artem Novikov <novikov@i-market.ru>
2017-07-31 17:39:57 +0300

    [our-work] use preview_text, resize thumbnails

--- public/local/templates/main/partials/our_work/thumbnail.php
+++ public/local/templates/main/partials/our_work/thumbnail.php
@@ -1,13 +1,15 @@
 <? use App\View as v; ?>
 
+<? // TODO editing actions ?>
 <? // TODO 4 columns as in the psd mockup? ?>
 <div class="item col col_3">
     <? // TODO add fancybox gallery id? ?>
     <? $link = v::get($item, 'DISPLAY_PROPERTIES.FILE.FILE_VALUE.SRC', $item['DETAIL_PICTURE']['SRC']) ?>
     <a class="gallery" href="<?= $link ?>" target="_blank">
                     <span class="wrap_img">
-                        <img src="<?= $item['DETAIL_PICTURE']['SRC'] ?>" alt="<?= $item['DETAIL_PICTURE']['ALT'] ?>">
+                        <? // TODO image size ?>
+                        <img src="<?= v::resize($item['DETAIL_PICTURE'], 370, 192) ?>" alt="<?= $item['DETAIL_PICTURE']['ALT'] ?>">
                     </span>
-        <span class="text"><?= $item['DETAIL_TEXT'] ?></span>
+        <span class="text"><?= !v::isEmpty($item['PREVIEW_TEXT']) ? $item['PREVIEW_TEXT'] : $item['DETAIL_TEXT'] ?></span>
     </a>
 </div>

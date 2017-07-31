9dc3dca390157f1f587f3dd6d65de12ea1eed818
Artem Novikov <novikov@i-market.ru>
2017-07-31 18:01:30 +0300

    [our-work] render empty galleries

--- public/local/templates/main/components/bitrix/catalog/our_work/bitrix/catalog.section/.default/template.php
+++ public/local/templates/main/components/bitrix/catalog/our_work/bitrix/catalog.section/.default/template.php
@@ -2,22 +2,20 @@
 
 use App\View as v;
 ?>
-<? if (!v::isEmpty($arResult['ITEMS'])): ?>
-    <div class="our-work-thumbnail-grid our-work-thumbnail-grid--page">
-        <div class="grid">
-            <? foreach ($arResult['ITEMS'] as $item): ?>
-                <?= v::render('partials/our_work/thumbnail', array_merge(get_defined_vars(), ['template' => $this])) ?>
-            <? endforeach ?>
-        </div>
+<div class="our-work-thumbnail-grid our-work-thumbnail-grid--page">
+    <div class="grid">
+        <? foreach ($arResult['ITEMS'] as $item): ?>
+            <?= v::render('partials/our_work/thumbnail', array_merge(get_defined_vars(), ['template' => $this])) ?>
+        <? endforeach ?>
     </div>
-    <? if (isset($arResult['PATH'][0]['SECTION_PAGE_URL'])): ?>
-        <div class="bottom_btn bottom_btn--back">
-            <a href="<?= $arResult['PATH'][0]['SECTION_PAGE_URL'] ?>" class="big_btn">
+</div>
+<? if (isset($arResult['PATH'][0]['SECTION_PAGE_URL'])): ?>
+    <div class="bottom_btn bottom_btn--back">
+        <a href="<?= $arResult['PATH'][0]['SECTION_PAGE_URL'] ?>" class="big_btn">
         <span class="img">
             <img src="<?= v::asset('images/arrow_left_white.svg') ?>">
         </span>
-                <span class="text"><span>Назад</span></span>
-            </a>
-        </div>
-    <? endif ?>
+            <span class="text"><span>Назад</span></span>
+        </a>
+    </div>
 <? endif ?>

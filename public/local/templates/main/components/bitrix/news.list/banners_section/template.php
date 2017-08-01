926a1768da8b935070054b5fd04f7381d9261cac
Artem Novikov <novikov@i-market.ru>
2017-08-01 10:45:57 +0300

    [banners section] open links in a new tab

--- public/local/templates/main/components/bitrix/news.list/banners_section/template.php
+++ public/local/templates/main/components/bitrix/news.list/banners_section/template.php
@@ -9,6 +9,7 @@ use App\View as v;
                 <? foreach ($arResult['ITEMS'] as $item): ?>
                     <? $link = $item['PROPERTIES']['LINK']['VALUE'] ?>
                     <a href="<?= $link ?>"
+                       target="_blank"
                        class="col col_4"
                        style="background: url('<?= $item['PREVIEW_PICTURE']['SRC'] ?>')no-repeat center center / cover"
                        id="<?= v::addEditingActions($item, $this) ?>"></a>

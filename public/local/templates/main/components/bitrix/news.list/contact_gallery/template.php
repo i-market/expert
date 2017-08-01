fe7c6f74c29ff2e0bcd0f4e7e7435fddfdf145a1
Artem Novikov <novikov@i-market.ru>
2017-08-01 11:40:33 +0300

    [contact gallery] use preview_picture if available

--- public/local/templates/main/components/bitrix/news.list/contact_gallery/template.php
+++ public/local/templates/main/components/bitrix/news.list/contact_gallery/template.php
@@ -5,8 +5,9 @@ use App\View as v;
 <div class="contacts_gallery">
     <div class="grid">
         <? foreach ($arResult['ITEMS'] as $item): ?>
+            <? $preview = !v::isEmpty($item['PREVIEW_PICTURE']) ? $item['PREVIEW_PICTURE'] : $item['DETAIL_PICTURE'] ?>
             <a class="gallery col col_3" href="<?= $item['DETAIL_PICTURE']['SRC'] ?>"
-               style="background: url('<?= $item['DETAIL_PICTURE']['SRC'] ?>')no-repeat center center / cover;"
+               style="background: url('<?= $preview['SRC'] ?>')no-repeat center center / cover;"
                id="<?= v::addEditingActions($item, $this) ?>"></a>
         <? endforeach ?>
     </div>

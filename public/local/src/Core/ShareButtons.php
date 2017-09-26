640b65cb097cf657cca30c41db0e4939a2bf0bc6
Artem Novikov <novikov@i-market.ru>
2017-09-26 19:38:32 +0300

    fix typo

--- public/local/src/Core/ShareButtons.php
+++ public/local/src/Core/ShareButtons.php
@@ -5,11 +5,11 @@ namespace Core;
 class ShareButtons {
     static function shareUrls($url, $text) {
         return [
-            'facebook' => "https://facebook.com/sharer/sharer.php?u=${url}",
-            'twitter' => "https://twitter.com/intent/tweet/?text=${text}&amp;url=${url}",
-            'odnoklassniki' => "https://ok.ru/dk?st.cmd=addShare&st.s=1&st._surl=${url}&st.comments=${text}",
-            'vkontakte' => "https://vk.com/share.php?title=${$text}&amp;url=${url}",
-            'google' => "https://plus.google.com/share?url=${url}"
+            'facebook' => "https://facebook.com/sharer/sharer.php?u={$url}",
+            'twitter' => "https://twitter.com/intent/tweet/?text={$text}&amp;url={$url}",
+            'odnoklassniki' => "https://ok.ru/dk?st.cmd=addShare&st.s=1&st._surl={$url}&st.comments={$text}",
+            'vkontakte' => "https://vk.com/share.php?title={$text}&amp;url={$url}",
+            'google' => "https://plus.google.com/share?url={$url}"
         ];
     }
 }

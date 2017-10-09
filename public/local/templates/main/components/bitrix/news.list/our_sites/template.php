55c6ce08967f356c89ba7f5dfb5179ccbfd70d5d
Artem Novikov <novikov@i-market.ru>
2017-10-09 13:38:32 +0300

    homepage our sites: shrink text elements to show more of the image, resize assets

--- public/local/templates/main/components/bitrix/news.list/our_sites/template.php
+++ public/local/templates/main/components/bitrix/news.list/our_sites/template.php
@@ -6,16 +6,18 @@ use App\View as v;
     <div class="grid">
         <? foreach ($arResult['ITEMS'] as $item): ?>
             <? $link = $item['PROPERTIES']['LINK']['VALUE'] ?>
-            <div class="item">
-                <div class="img"
-                     style="background: url('<?= $item['PREVIEW_PICTURE']['SRC'] ?>')no-repeat center center / cover"
-                     id="<?= v::addEditingActions($item, $this) ?>"></div>
-                <div class="info">
-                    <p><?= $item['PREVIEW_TEXT'] ?></p>
+            <div class="slide">
+                <div class="item">
+                    <div class="img"
+                         style="background: url('<?= v::resize($item['PREVIEW_PICTURE'], 500, 500) ?>')no-repeat center center / cover"
+                         id="<?= v::addEditingActions($item, $this) ?>"></div>
+                    <div class="info">
+                        <p><?= $item['PREVIEW_TEXT'] ?></p>
+                    </div>
+                    <? if (!v::isEmpty($link)): ?>
+                        <a href="<?= $link ?>"></a>
+                    <? endif ?>
                 </div>
-                <? if (!v::isEmpty($link)): ?>
-                    <a href="<?= $link ?>"></a>
-                <? endif ?>
             </div>
         <? endforeach ?>
     </div>

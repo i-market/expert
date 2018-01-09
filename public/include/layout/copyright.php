479b0c52fad30099386a0726251d335dbd6a1815
Artem Novikov <novikov@i-market.ru>
2018-01-09 18:24:16 +0300

    footer: add yandex informer

--- public/include/layout/copyright.php
+++ public/include/layout/copyright.php
@@ -1,1 +1,10 @@
 <span>© 2015–<?= date('Y') ?></span> <span>ООО «Техническая строительная экспертиза»</span>
+<!-- Yandex.Metrika informer -->
+<span>
+<a href="https://metrika.yandex.ru/stat/?id=22761460&amp;from=informer"
+   target="_blank" rel="nofollow"><img src="<?= \App\App::env() === \Core\Env::DEV
+        ? '/images/mock-yandex-informer.png'
+        : 'https://informer.yandex.ru/informer/22761460/3_1_EFEFEFFF_EFEFEFFF_0_pageviews' ?>"
+style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" /></a>
+</span>
+<!-- /Yandex.Metrika informer -->

4e1b1e4562ec44cd675ad8e574cc128556846981
Artem Novikov <novikov@i-market.ru>
2018-01-16 10:24:58 +0300

    update ravenjs, use their cdn

--- public/local/templates/main/partials/sentry_js.php
+++ public/local/templates/main/partials/sentry_js.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/raven.js/3.12.1/raven.min.js"></script>
+<script src="https://cdn.ravenjs.com/3.22.1/raven.min.js" crossorigin="anonymous"></script>
 <script type="text/javascript">
     Raven.config('<?= $publicDsn ?>', {
         environment: '<?= $env ?>'
